2917aa2bd6c5c9af982551e7540016866bec4d35
Bhavana K M <bhavanakm147@gmail.com>
2025-12-04 21:18:54 +0530

    Storing the data using local Storage

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
-let input = document.getElementById('taskInput');
-let list = document.getElementById('taskList');
+let input = document.getElementById("taskInput");
+let list = document.getElementById("taskList");
 
-input.addEventListener('keypress', function(event) {
-    if (event.key === 'Enter' && input.value.trim() !== '') {
-        let listItem = document.createElement('li');    
-        listItem.textContent = input.value.trim();
-        list.appendChild(listItem);
-        input.value = '';
-    }   
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+//old tasks 
+tasks.forEach(task => {
+  let li = document.createElement("li");
+  li.textContent = task;
+  list.appendChild(li);
+});
+
+//add new task 
+input.addEventListener("keydown", function (e) {
+  if (e.key === "Enter" && input.value !== "") {
+    let task = input.value;
+
+    tasks.push(task);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+
+    let li = document.createElement("li");
+    li.textContent = task;
+    list.appendChild(li);
+
+    input.value = "";
+  }
 });
